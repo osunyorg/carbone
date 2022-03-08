2f611acc589a41f76642d75dc09725f32c31917d
alexisben <alexiben7@gmail.com>
2022-03-08 15:46:43 +0100

    Reorder actions

--- assets/js/charts/universities.js
+++ assets/js/charts/universities.js
@@ -1,13 +1,26 @@
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
+// import pSBC from './psbc';
+
 export const makeUniversitiesChart = function () {
     const table = document.querySelector('.table'),
-        tons = [];
+        tons = [],
+        COLORS = {
+            green: '#55C461',
+            grey: 'rgb(225,225,225)',
+            white: 'rgb(255,255,255)'
+        };
+
     let name,
         tonsPerYear,
         osunyTonsPerYear,
-        tonsSaved = 0;
+        tonsSaved = 0,
+        buttons,
+        chart,
+        actions;
+        // totalTons = 0,
+        // averageTons = 0;
 
     table.querySelectorAll('tr').forEach((tr, i) => {
         name = tr.getAttribute('data-name');
@@ -22,24 +35,36 @@ export const makeUniversitiesChart = function () {
             label: name,
             legend: false,
             data: [tonsPerYear, osunyTonsPerYear],
-            backgroundColor: i % 2 === 0 ? 'rgb(255,255,255)' : 'rgb(225,225,225)',
+            backgroundColor: i % 2 === 0 ? COLORS.white : COLORS.grey,
             stack: 'tons',
             fill: true,
+            index: i,
             order: 1
         });
+
+        // totalTons += tonsPerYear;
     });
 
     tons.push({
         label: 'Gain de sobriété',
         legend: false,
         data: [0, tonsSaved],
-        backgroundColor: '#55C461',
+        backgroundColor: COLORS.green,
         stack: 'tons',
         fill: true,
+        id: 'saved',
         order: 1
     });
 
-    new Chart(document.getElementById('chart'), {
+    // Set impact color :
+    // averageTons = totalTons / tons.length;
+
+    // tons.forEach((data) => {
+    //     let lighten = 1 - (data.data[0] / totalTons) * 20;
+    //     data.backgroundColor = pSBC(lighten, '#DD3333');
+    // });
+
+    chart = new Chart(document.getElementById('chart'), {
         type: 'bar',
         data: {
             labels: ['Tonnes eq CO2 des sites existants', 'Tonnes eq CO2 avec Osuny'],
@@ -66,6 +91,40 @@ export const makeUniversitiesChart = function () {
                     display: false
                 }
             }
+        }
+    });
+
+    // Handle actions
+    const resetColors = function () {
+        chart.data.datasets.forEach((data, i) => {
+            if (data.id !== 'saved') {
+                data.backgroundColor = i % 2 === 0 ? COLORS.white : COLORS.grey;
+            }
+        });
+    };
+
+    buttons = document.querySelectorAll('[data-chart-action]');
+    actions = {
+        orderByTons: () => {
+            chart.data.datasets.sort((a, b) => {
+                return b.data[0] - a.data[0];
+            });
+            resetColors();
+            chart.update();
+        },
+        orderByName: () => {
+            chart.data.datasets.sort((a, b) => {
+                return a.index - b.index;
+            });
+            resetColors();
+            chart.update();
+        }
+    };
+
+    buttons.forEach((button) => {
+        let action = button.getAttribute('data-chart-action');
+        if (actions[action]) {
+            button.addEventListener('click', actions[action]);
         }
     });
 };
